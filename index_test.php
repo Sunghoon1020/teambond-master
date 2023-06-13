ed0575228642ea19479aacc76ee8ba12c1013895
Sunghoon1020 <132966271+Sunghoon1020@users.noreply.github.com>
2023-06-12 18:53:52 -0700

    Add files via upload

--- index_test.php
+++ index_test.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
   <head>
@@ -7,32 +6,7 @@
     <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
 
-    <style>
-    .footer{
-      background-color: #333;
-      color: white;
-      padding: 10px;
-      text-align: center;
-      position: fixed;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-    }  
-    .login-box {
-      width: 320px;
-      height: 420px;
-      background: #fff;
-      color: #000;
-      top: 70%;
-      left: 50%;
-      position: absolute;
-      transform: translate(-50%,-50%);
-      box-sizing: border-box;
-      padding: 150px 30px;
-      border-radius: 10px;
-    }
-    </style>
-
+    
   </head>
   <body>
     <header>
@@ -44,10 +18,9 @@
         <p>This is the home page of TeamBond App.</p>
       </div>
 
-      <div class="login-box" id="login" style="height:100%">
+      <div class="login-box" id="login">
         <h1>Login</h1>
-
-        <form  role="form" action="login.php" method="post" name="login">
+        <form action="login.php" method="post" name="login">
           <label for="username">Username:</label>
           <input type="text" id="username" name="user_name" placeholder="Enter username" required>
     
@@ -58,9 +31,14 @@
           <a href="./registration.php"><button type="button">Sign up</button></a>
         </form>
       </div>
+
+      <div id="profile">
+        Welcome, Admin!
+      </div>
     </main>
+  
     
-    <footer class="footer">
+    <footer>
       <p>&copy; 2023 Westcliff University</p>
     </footer>
   </body>
